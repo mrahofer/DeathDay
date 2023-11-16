be1b0535bbe3e4a1d77ab051e898f92147627f78
mrahofer <mrahofer@htl-steyr.ac.at>
2023-11-16 11:48:27 +0100

    TestingClass added

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @DisplayName("Test nothing")
+    void main(){
+        assertNotEquals(2, 3);
+    }
+}
