07fadcc975f33f182c695fcd4fc818e5bcb1c494
mrahofer <mrahofer@htl-steyr.ac.at>
2023-11-16 11:10:23 +0100

    initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("... " +name +" will die on " );
+    }
+}
